6c292afaab8860717fd4bd49793ce83272813749
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2024-11-12 11:42:40 +0000

    Updated run scripts

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -5,8 +5,10 @@ source $(ccc file_cfg)
 source $(ccc file_utils)
 
 export USE_CONDA_ENV=unified-io-2
-export DISABLE_X11=0
+export DISABLE_X11=1
 
+export SLURM_JOB_ARGS="--time=5:00:00 --mem-per-gpu=192G --partition=frida --cpus-per-task=32" # HPC-FRIDA # GPU_BRD:H100, GPU_MEM:80GB
+export SLURM_JOB_ARGS="$SLURM_JOB_ARGS --output=/dev/null --error=/dev/null "
 #GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=1 --hosts="crushinator(1+2+3+4)" --gpus_as_single_host=True)
 GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=3 --hosts="crushinator,kif,zapp" --gpus_as_single_host=True)
 
@@ -15,11 +17,12 @@ GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=3 --hosts=
 #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=test eval_epoch=_100 &
 #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=test eval_epoch=_050 &
 
-ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=test eval_epoch=_025 &
-ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=test eval_epoch=_075 &
-ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=test eval_epoch=_150 &
-
 #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=train eval_epoch="" &
+
+for epoch in _005 _010 _015 _020 _025 _030 _035 _040 _045 ""; do #
+    #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5 eval_type=test eval_epoch=$epoch skip_if_exists=True &
+    ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5 eval_type=train eval_epoch=$epoch skip_if_exists=True &
+done
 
 wait_or_interrupt
 rm $GPUS_FILE
--- scripts/run_finetuning2.sh
+++ scripts/run_finetuning2.sh
@@ -23,11 +23,18 @@ export DISABLE_X11=0
 export SLURM_JOB_ARGS="--time=72:00:00 --mem-per-gpu=128G --partition=frida --cpus-per-task=32 --constraint=GPU_MEM:80GB" # HPC-FRIDA # GPU_BRD:H100
 GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=1 --hosts="crushinator(1+2+3+4)" --gpus_as_single_host=True)
 
+# with keypoint order randomization and weight decay
 #ccc run $GPUS_FILE python -m train ../config/vicos_towel/train.py --config  n_epochs=100 train_dataset.batch_size=16 train_dataset.workers=16 model.lr=1e-3 model.accumulate_grads_iter=8 train_dataset.kwargs.num_cpu_threads=4
 #ccc run $GPUS_FILE python -m train ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 model.weight_decay=1e-5 n_epochs=100 save_interval=10 extra_str="_weight_decay=1e-5" &
 #ccc run $GPUS_FILE python -m train ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 model.weight_decay=1e-6 n_epochs=100 save_interval=10 extra_str="_weight_decay=1e-6" &
 
-ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5  &
+# with MUJOCO and with keypoint order randomization
+#ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5  &
+
+# with MUJOCO and with keypoint order randomization
+ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=3 extra_str="_keypoint_jitter=3"  &
+ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=5 extra_str="_keypoint_jitter=5"  &
+ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=7 extra_str="_keypoint_jitter=7"  &
 
 
 wait_or_interrupt
