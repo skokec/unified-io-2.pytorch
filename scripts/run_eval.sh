2ab8fedfc11397d7a40fc1c728417f5df73133f2
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2024-11-13 12:43:21 +0000

    Updated eval and finetune script

--- scripts/run_eval.sh
+++ scripts/run_eval.sh
@@ -8,6 +8,7 @@ export USE_CONDA_ENV=unified-io-2
 export DISABLE_X11=1
 
 export SLURM_JOB_ARGS="--time=5:00:00 --mem-per-gpu=192G --partition=frida --cpus-per-task=32" # HPC-FRIDA # GPU_BRD:H100, GPU_MEM:80GB
+#export SLURM_JOB_ARGS="--time=1:00:00 --mem-per-gpu=32G --partition=dev --cpus-per-task=32" # HPC-FRIDA # GPU_BRD:H100, GPU_MEM:80GB
 export SLURM_JOB_ARGS="$SLURM_JOB_ARGS --output=/dev/null --error=/dev/null "
 #GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=1 --hosts="crushinator(1+2+3+4)" --gpus_as_single_host=True)
 GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=5 --hosts="crushinator,kif,zapp" --gpus_as_single_host=True)
@@ -22,9 +23,17 @@ GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=5 --hosts=
 
 #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=300 save_interval=25 eval_type=train eval_epoch="" &
 
-for epoch in _005 _010 _015 _020 _025 _030 _035 _040 _045 ""; do #
-    #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5 eval_type=test eval_epoch=$epoch skip_if_exists=True &
-    ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5 eval_type=train eval_epoch=$epoch skip_if_exists=True &
+for epoch in _005 _010 _015 _020 _025 _030  "" ; do # _035 _040 _045
+    for eval_cropped in False True; do
+        ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=3 extra_str="_keypoint_jitter=3" eval_type=test eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True  &
+        #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=3 extra_str="_keypoint_jitter=3" eval_type=train eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True &
+
+        ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=5 extra_str="_keypoint_jitter=5" eval_type=test eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True  &
+        #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=5 extra_str="_keypoint_jitter=5" eval_type=train eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True &
+
+        ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=7 extra_str="_keypoint_jitter=7" eval_type=test eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True  &
+        #ccc run $GPUS_FILE python -m test_vicos_towel ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=7 extra_str="_keypoint_jitter=7" eval_type=train eval_epoch=$epoch eval_cropped=$eval_cropped skip_if_exists=True &
+    done
 done
 
 wait_or_interrupt
--- scripts/run_finetuning2.sh
+++ scripts/run_finetuning2.sh
@@ -21,6 +21,7 @@ export DISABLE_X11=0
 
 #export SLURM_JOB_ARGS="--output=logs/%j-node-%t.out --time=72:00:00 --mem-per-gpu=128G --partition=gpu --cpus-per-task=32 --constraint=h100" # HPC-ARNES
 export SLURM_JOB_ARGS="--time=72:00:00 --mem-per-gpu=128G --partition=frida --cpus-per-task=32 --constraint=GPU_MEM:80GB" # HPC-FRIDA # GPU_BRD:H100
+export SLURM_JOB_ARGS="$SLURM_JOB_ARGS --output=/dev/null --error=/dev/null "
 GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=1 --hosts="crushinator(1+2+3+4)" --gpus_as_single_host=True)
 
 # with keypoint order randomization and weight decay
@@ -31,11 +32,14 @@ GPUS_FILE=$(ccc gpus --on_cluster=$CLUSTER_INFO_FILE --gpus=1 --tasks=1 --hosts=
 # with MUJOCO and with keypoint order randomization
 #ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=50 save_interval=5  &
 
-# with MUJOCO and with keypoint order randomization
-ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=3 extra_str="_keypoint_jitter=3"  &
-ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=5 extra_str="_keypoint_jitter=5"  &
-ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=7 extra_str="_keypoint_jitter=7"  &
+# with MUJOCO, with keypoint order randomization and keypoint location jitter
+#ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=3 extra_str="_keypoint_jitter=3"  &
+#ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=5 extra_str="_keypoint_jitter=5"  &
+#ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.jitter_keypoints_px=7 extra_str="_keypoint_jitter=7"  &
 
+# with internal scale augmentation during training and keypoint order randomization (with and without MuJoCo)
+ccc run $GPUS_FILE python -m train ../config/vicos_towel+mujoco/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=35 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.apply_internal_scale_aug=True extra_str="_internal_scale_aug"  &
+ccc run $GPUS_FILE python -m train ../config/vicos_towel/train.py --config train_dataset.batch_size=24 train_dataset.workers=16 model.lr=1e-3 n_epochs=100 save_interval=5 train_dataset.keypoint_preprocesser_kwargs.apply_internal_scale_aug=True extra_str="_internal_scale_aug"  &
 
 wait_or_interrupt
 rm $GPUS_FILE
