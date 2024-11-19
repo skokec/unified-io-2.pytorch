5580a48f1d1886cb2188802ccc84410f350e4cc7
Domen Tabernik <domen.tabernik@fri.uni-lj.si>
2024-11-19 18:31:23 +0100

    Added mask target to KeypointPreprocessorDataset and implemented defintion of GT key from config

--- datasets/PreprocessorDataset.py
+++ datasets/PreprocessorDataset.py
@@ -26,9 +26,11 @@ def centers_to_tokens(gt_centers, img_shape):
 
     return gt_centers_text
 
+
+
 class KeypointPreprocessorDataset(Dataset):
 
-    def __init__(self, preprocessor, dataset, returned_raw_sample=False, randomize_keypoints_order=True, jitter_keypoints_px=False, apply_internal_scale_aug=False):
+    def __init__(self, preprocessor, dataset, targets_definition, returned_raw_sample=False, randomize_keypoints_order=True, jitter_keypoints_px=False, apply_internal_scale_aug=False):
         self.preprocessor = preprocessor
         self.dataset = dataset
 
@@ -40,6 +42,15 @@ class KeypointPreprocessorDataset(Dataset):
         self.jitter_keypoints_px = jitter_keypoints_px
         self.apply_internal_scale_aug = apply_internal_scale_aug
 
+        if targets_definition is None:
+            # set to use keypoints by default
+            targets_definition = [dict(type='keypoints', prob=1.0,
+                                    groundruth_key='center',
+                                    prompt_key='Towel_Corners')]
+
+        self.targets_definition = targets_definition 
+        self.targets_types, self.targets_prob = zip(*[(t['type'], t['prob']) for t in self.targets_definition])
+
     def __len__(self):
         return len(self.dataset)
     
@@ -49,67 +60,46 @@ class KeypointPreprocessorDataset(Dataset):
         return self.preprocess_sample(sample)
     
     def preprocess_sample(self, sample):
-        img = sample['image']
-
-        # get gt points
-        center = sample['center']
         
-        # use this function to create keypoints based on actual image translation and convert them to text_target
-        def translate_gt(features, center):
-            from uio2 import config            
-            gt_centers = center[(center[:, 0] > 0) | (center[:, 1] > 0), :]
-            if len(gt_centers) == 0:
-                features['text_targets'] = "No points found"
-                return features
-            # convert them to transformed input image space
-            top_pad, left_pad, scale, in_height, in_width,_,_,off_y,off_x,_,_ = np.array(features['meta/image_info'])
-            # add scale            
-            gt_centers /= scale
-
-            # add padding
-            gt_centers[:,0] += left_pad - off_x
-            gt_centers[:,1] += top_pad - off_y
-
-            # swicth x,y 
-            gt_centers = gt_centers[:,[1,0]]
-
-            # remove keypoints that could fall outside of image (e.g., due to internal scale augmentation that also crops the image)
-            gt_centers = np.array([[x,y] for x,y in gt_centers if x >=0 and x<config.IMAGE_INPUT_SIZE[1]-1 and y >=0 and y<config.IMAGE_INPUT_SIZE[0]-1])
-
-            if len(gt_centers) == 0:
-                features['text_targets'] = "No points found"
-                return features
-
-            if self.randomize_keypoints_order:
-                # Randomly shuffle the order of keypoints
-                indices = np.random.permutation(gt_centers.shape[0])
-                gt_centers = gt_centers[indices]
-
-            if self.jitter_keypoints_px:
-                # jitter each keypoint to prevent overfitting to the exact values
-
-                # Generate random jitter for each coordinate (N, 2)
-                jitter = np.random.uniform(-self.jitter_keypoints_px, self.jitter_keypoints_px, size=gt_centers.shape)
-
-                # Add the jitter to the original centers
-                gt_centers += jitter
-
-                # clip it within range
-                gt_centers[:,0] = np.clip(gt_centers[:,0],0, config.IMAGE_INPUT_SIZE[1]-1)
-                gt_centers[:,1] = np.clip(gt_centers[:,1],0, config.IMAGE_INPUT_SIZE[0]-1)
+        img = sample['image']  
+        # prepare args for preprocessor
+        preprocessor_args = dict(image_inputs=np.transpose(img,(1,2,0)),
+                                 is_training=self.apply_internal_scale_aug,)
 
 
-            gt_centers_text = centers_to_tokens(gt_centers, config.IMAGE_INPUT_SIZE)
-            features['text_targets'] = gt_centers_text
+        # we can only process one target per sample, randomly chose one based on probabilities
+        selected_type = np.random.choice(self.targets_types, p=self.targets_prob)
 
-            return features
 
-        from functools import partial
+        selected_target = self.targets_definition[selected_type]
+   
+        if selected_target['type'].lower() == 'keypoints':
+            gt_key = selected_target['groundruth_key']
+            prompt_key = selected_target['prompt_key']
+
+
+            # set text target and set function to generate text from keypoints
+            from functools import partial
+            preprocessor_args.update(dict(text_targets='', 
+                                          target_modality='text',
+                                          raw_features_fn=partial(self._generate_keypoint_target, keypoints=sample[gt_key])))
+                                         
+
+        elif selected_target['type'].lower() == 'mask':
+            gt_key = selected_target['groundruth_key'] 
+            prompt_key = selected_target['prompt_key'] 
+
+            # set image target and set function to generate text from keypoints
+            preprocessor_args.update(dict(image_targets=sample[gt_key], 
+                                          target_modality='image'))
+        else:
+            raise Exception("Invalid target type for KeypointPreprocessorDataset (only 'keypoints' or 'mask' allowed)")
+
+        # generate appropriate input prompt
+        input = self.prompt.random_prompt(prompt_key)
         
-        input = self.prompt.random_prompt('Towel_Corners')
-
-        preprocessed_example = self.preprocessor(text_inputs=input, image_inputs=np.transpose(img,(1,2,0)), text_targets="", target_modality="text", 
-                                                 is_training=self.apply_internal_scale_aug, raw_features_fn=partial(translate_gt, center=center),)
+        # do preprocessing
+        preprocessed_example = self.preprocessor(text_inputs=input, **preprocessor_args)
         
         if 'index' in sample:
             preprocessed_example['index'] = sample['index']
@@ -122,6 +112,56 @@ class KeypointPreprocessorDataset(Dataset):
             preprocessed_example['sample'] = sample
 
         return preprocessed_example
+
+    # use this function to create keypoints based on actual image translation and convert them to text_target
+    def _generate_keypoint_target(self, features, keypoints):
+        from uio2 import config            
+        gt_centers = keypoints[(keypoints[:, 0] > 0) | (keypoints[:, 1] > 0), :]
+        if len(gt_centers) == 0:
+            features['text_targets'] = "No points found"
+            return features
+        # convert them to transformed input image space
+        top_pad, left_pad, scale, in_height, in_width,_,_,off_y,off_x,_,_ = np.array(features['meta/image_info'])
+        # add scale            
+        gt_centers /= scale
+
+        # add padding
+        gt_centers[:,0] += left_pad - off_x
+        gt_centers[:,1] += top_pad - off_y
+
+        # swicth x,y 
+        gt_centers = gt_centers[:,[1,0]]
+
+        # remove keypoints that could fall outside of image (e.g., due to internal scale augmentation that also crops the image)
+        gt_centers = np.array([[x,y] for x,y in gt_centers if x >=0 and x<config.IMAGE_INPUT_SIZE[1]-1 and y >=0 and y<config.IMAGE_INPUT_SIZE[0]-1])
+
+        if len(gt_centers) == 0:
+            features['text_targets'] = "No points found"
+            return features
+
+        if self.randomize_keypoints_order:
+            # Randomly shuffle the order of keypoints
+            indices = np.random.permutation(gt_centers.shape[0])
+            gt_centers = gt_centers[indices]
+
+        if self.jitter_keypoints_px:
+            # jitter each keypoint to prevent overfitting to the exact values
+
+            # Generate random jitter for each coordinate (N, 2)
+            jitter = np.random.uniform(-self.jitter_keypoints_px, self.jitter_keypoints_px, size=gt_centers.shape)
+
+            # Add the jitter to the original centers
+            gt_centers += jitter
+
+            # clip it within range
+            gt_centers[:,0] = np.clip(gt_centers[:,0],0, config.IMAGE_INPUT_SIZE[1]-1)
+            gt_centers[:,1] = np.clip(gt_centers[:,1],0, config.IMAGE_INPUT_SIZE[0]-1)
+
+
+        gt_centers_text = centers_to_tokens(gt_centers, config.IMAGE_INPUT_SIZE)
+        features['text_targets'] = gt_centers_text
+
+        return features
         
 
 
